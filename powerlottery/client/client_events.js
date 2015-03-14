f54f28a20015bcad5fd83e6b650925fde94ab0f8
Junwei Yu <junweiyu@berkeley.edu>
2015-03-14 03:24:41 -0700

    fixed register form to check device database from server

--- powerlottery/client/client_events.js
+++ powerlottery/client/client_events.js
@@ -73,38 +73,36 @@ Template._sideMenu.events({
     name = t.find('#account-name').value,
     password = t.find('#account-password').value,
     d_id = "" + t.find('#device-id').value + "";
-
+      
     // Check if device is in the device database
-    var lookInDevices = devices.find({_id: d_id}).count();
-    // Device in database. Valid device. Now check if it's already been claimed
-
-    if (lookInDevices > 0) {
-      // get the current device info
-      var register_info = devices.find(
-        {_id:d_id},
-        { email: 1, status:1}).fetch();
-        if (register_info[0].status == 0) {
-          // unregistered valid device, continue
-          Accounts.createUser({email: email, password : password, profile: {name: name, device_id: d_id,
-            datejoined: new Date(), offers_completed: 0, total_tokens:0, current_offer_endtime: 0}}, function(err){
-              if (err) {
-                console.log(err);
-                Session.set('response_msg', "Account creation failed");
-                }
+    Meteor.call("realdeviceid", d_id, function(err, data){
+      if (data > 0) {
+        // Device in database. Valid device. Now check if it's already been claimed
+        Meteor.call("isAvail", d_id, function(err, data){
+          if (data == 0) {
+            // unregistered valid device, continue
+            Accounts.createUser({email: email, password : password, profile: {name: name, device_id: d_id,
+              datejoined: new Date(), offers_completed: 0, total_tokens:0, current_offer_endtime: 0}}, function(err){
+                if (err) {
+                  console.log(err);
+                  Session.set('response_msg', "Account creation failed");
+                  }
                 else {
                   console.log("successfully added user");
                   // TODO: update with status of ac
                   devices.update({_id: d_id}, {$set: {status:1, uid: Meteor.userId()}});
-                  }
-                });
-              }
-        else {
-          Session.set('response_msg', "This device has already been registered!");
+                }
+              });
           }
-        }
-    else if (lookInDevices == 0) {
-      Session.set('response_msg', "Sorry, this device doesn't exist!");
-    }
+          else {
+            Session.set('response_msg', "This device has already been registered!" + status);
+          }
+        });
+      }
+      else {
+          Session.set('response_msg', "Sorry, this device doesn't exist!" + lookInDevices);
+      }
+    });
   }
-});
+ });
 }
