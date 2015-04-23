850aaebb19ec4100e1a9ebc3aa5ac12964ac21b6
jessica wong <jesca116@berkeley.edu>
2015-04-23 10:09:06 -0700

    fixed some stuff on router

--- powerlottery/shared/router.js
+++ powerlottery/shared/router.js
@@ -1,3 +1,23 @@
+Router.onBeforeAction(function () {
+  // all properties available in the route function
+  // are also available here such as this.params
+  var routeName = this.route.getName();
+  console.log("route name" + this.route.getName());
+  if (!Meteor.user()) {
+    console.log("not logged in");
+    if (_.include(['userAccounts'], routeName)){
+        this.next();
+        return;
+      }
+    // if the user is not logged in, render the Login template
+    this.render('login');
+  } else {
+    // otherwise don't hold up the rest of hooks or our route/action function
+    // from running
+    this.next();
+  }
+});
+
 Router.configure({
   layoutTemplate:"layout"
 });
