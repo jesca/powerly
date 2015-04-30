b211b873f66820aa4a4fdc6a677e07159a66401d
jessica wong <jesca116@berkeley.edu>
2015-04-30 14:59:30 -0700

    made changes to history

--- powerlottery/client/js/client_helpers.js
+++ powerlottery/client/js/client_helpers.js
@@ -96,6 +96,46 @@ Template.history_list.helpers({
   },
   timeOffer: function() {
     d = new Date(this.end_time);
-    return d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear();
+    var month;
+    switch (d.getMonth() + 1) {
+    case 1:
+        month = "January";
+        break;
+    case 2:
+        month = "February";
+        break;
+    case 3:
+        month = "March";
+        break;
+    case 4:
+        month = "April";
+        break;
+    case 5:
+        month = "May";
+        break;
+    case 6:
+        month = "June";
+        break;
+    case 7:
+        month  = "July";
+        break;
+    case 8:
+        month = "August";
+        break;
+    case 9:
+        month = "September";
+        break;
+    case 10:
+        month = "October";
+        break;
+    case 11:
+        month = "November";
+        break;
+    case 12:
+        month = "December";
+        break;
+
+}
+    return '' + month + ' ' + (d.getDay()) + ', ' + d.getFullYear();
   }
 });
