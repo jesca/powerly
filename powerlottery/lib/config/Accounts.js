230a9334fc9eec169679151883bdd57c2eb3c397
jessica wong <jesca116@berkeley.edu>
2015-05-03 23:47:36 -0700

    hi score update

--- powerlottery/lib/config/Accounts.js
+++ powerlottery/lib/config/Accounts.js
@@ -24,7 +24,7 @@ AccountsTemplates.configure({
     lowercaseUsername: false,
 
     // Appearance
-    showForgotPasswordLink: true,
+    showForgotPasswordLink: false,
     showLabels: true,
 
     // Client-side Validation
