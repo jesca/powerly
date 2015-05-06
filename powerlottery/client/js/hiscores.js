e2a5fe193cc1a21fb20e4290d65d5c6fdfe91d75
jessica wong <jesca116@berkeley.edu>
2015-05-05 21:18:58 -0700

    changed high score list

--- powerlottery/client/js/hiscores.js
+++ powerlottery/client/js/hiscores.js
@@ -1,5 +1,14 @@
 Template.hiscores.helpers({
   scores: function() {
-    return Meteor.users.find({}, {sort: {total_tokens: -1}, limit: 10}).fetch();
+    return Meteor.users.find({}, {
+      sort: {
+        total_tokens: 1
+      },
+      limit: 10
+    }).map(function(user, index) {
+      user.rank = index + 1;
+      return user;
+    });
   }
+
 });
