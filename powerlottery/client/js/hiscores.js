37ba1f8574d811bcd1d10d3333d857a5d26409f9
Paymon <paymon.p@berkeley.edu>
2015-05-05 17:14:41 -0700

    Fixed android keyboard bug

--- powerlottery/client/js/hiscores.js
+++ powerlottery/client/js/hiscores.js
@@ -1,5 +1,11 @@
 Template.hiscores.helpers({
   scores: function() {
     return Meteor.users.find({}, {sort: {total_tokens: -1}, limit: 10}).fetch();
-  }
+  },
+  ac_on: function() {
+    return Meteor.user().profile.status == 1;
+  },
+  ac_disconnected: function() {
+    return Meteor.user().profile.status == 2;
+  },
 });
